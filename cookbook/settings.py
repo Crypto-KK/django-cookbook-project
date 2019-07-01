4d46132c777690a9c3bea4eca20a9903f28924fc
kk <705555262@qq.com>
2019-07-01 16:52:02 +0800

    Initial commit

---
+++ cookbook/settings.py
@@ -0,0 +1,10 @@
+from .config.dev import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-project',
+        'USER': 'root',
+        'PASSWORD': '998219',
+    }
+}
